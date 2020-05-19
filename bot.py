5cb050788886de6706040df108c2dc63b1daa2f6
Nigel <aquanat12@gmail.com>
2020-05-19 23:57:59 +0800

    Credit Card fill in should work now
    I hope

--- bot.py
+++ bot.py
@@ -8,33 +8,41 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver import ActionChains
+import pyautogui
+import csv
+import pandas as pd
+
 geckopath = "./geckodriver.exe"
+
+
 
 def Bot():
 
-    driver = webdriver.Firefox(executable_path=geckopath)
-
-    driver.get(
-        "https://undefeated.com/collections/all/products/undefeated-anointed-s-s-tee?variant=31341462945865")
-
-
-    first = "nigel"
-    last = "test"
-    number = "202-555-0183"
-    address = "test street"
-    city = "ALCESTER"
-    zip = "57001"
-    color = "BLACK"
-    size = "L"
-    loc = "US"
-    state = "SD"
-    uid = ""
-    upw = ""
-    cc = "357321382046786"
-    ed = "02/22"
-    cvv = "5555"
     
-   
+    # info = ["first name", "last name", "number", "address", "city", "zip", "color", "size", "location", "state", "Login", "Password", "cc", "ed", "cvv", "ccname"]
+    df = pd.read_csv("info.csv")
+    first = str(df["first name"]).strip(" 0").replace("Name: first name, dtype: object", '').strip("\n").strip("\n")
+    last = str(df["last name"]).strip(" 0").replace("Name: last name, dtype: object", '').strip("\n")
+    number = str(df["number"]).strip(" 0").replace("Name: number, dtype: object", '').strip("\n")
+    address = str(df["address"]).strip(" 0").replace("Name: address, dtype: object", '').strip("\n")
+    city = str(df["city"]).strip(" 0").replace("Name: city, dtype: object", '').strip("\n")
+    zip = str(df["zip"]).strip(" 0").replace("Name: zip, dtype: object", '').strip("\n")
+    color = str(df["color"]).strip(" 0").replace("Name: color, dtype: object", '').strip("\n")
+    size = str(df["size"]).strip(" 0").replace("Name: size, dtype: object", '').strip("\n")
+    loc = str(df["location"]).strip(" 0").replace("Name: location, dtype: object", '').strip("\n")
+    state = str(df["state"]).strip(" 0").replace("Name: state, dtype: object", '').strip("\n")
+    uid = str(df["Login"]).strip(" 0").replace("Name: Login, dtype: object", '').strip("\n")
+    upw = str(df["Password"]).strip(" 0").replace("Name: Password, dtype: object", '').strip("\n")
+    cc = str(df["cc"]).strip(" 0").replace("Name: cc, dtype: object", '').strip("\n")
+    ed = str(df["ed"]).strip(" 0").replace("Name: ed, dtype: object", '').strip("\n")
+    cvv = str(df["cvv"]).strip(" 0").replace("Name: cvv, dtype: object", '').strip("\n")
+    ccname = str(df["ccname"]).strip(" 0").replace("Name: ccname, dtype: object", '').strip("\n")
+    
+
+    driver = webdriver.Firefox(executable_path=geckopath)
+    driver.get("https://undefeated.com/collections/all/products/undefeated-racquet-s-s-tee?variant=31341463830601")
+
     ChangeShipping = WebDriverWait(driver, 60).until(
         EC.element_to_be_clickable((By.XPATH, "/html/body/div[6]/div/div/div/div/div/div[2]/div[3]/a"))
     )
@@ -128,12 +136,14 @@ def Bot():
     hp.send_keys(number)
     
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    
 
     print("Solve the captcha now!\n\n")
     captcha_solved = "1"
     while captcha_solved == "1":
         captcha_solved = input("[Y]Solved?\n")
-
+    
+    pyautogui.hotkey('alt', 'tab', interval=0.1)
 
     submit = WebDriverWait(driver, 60).until(
         EC.element_to_be_clickable((By.ID, 'continue_button'))
@@ -152,36 +162,30 @@ def Bot():
     print("\n\n[*] UPS Ground Selected")
 
     # Page 3
+    
+    time.sleep(3)
+    
+    for i in range(8):
+        pyautogui.press('tab')
+        time.sleep(0.1)
 
-    input_cc = WebDriverWait(driver, 60).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, '#number'))
-    )
-    input_cc.click()
-    input_cc.send_keys(cc)
+    for i in range(16):
+        pyautogui.typewrite(cc[i])
+        time.sleep(0.1)
+    time.sleep(1)
+    pyautogui.press('tab')
+    pyautogui.write(ccname)
+    time.sleep(1)
+    pyautogui.press('tab')
+    pyautogui.write(ed)
+    time.sleep(1)
+    pyautogui.press('tab')
+    pyautogui.write(cvv)
+    
 
-    input_name = WebDriverWait(driver, 60).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, '#name'))
-    )
-    input_name.click()
-    input_name.send_keys(first)
+    print("\n\n[*] Bought!")
 
-    input_ed = WebDriverWait(driver, 60).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, '#expiry'))
-    )
-    input_ed.send_keys(ed)
-
-    input_ccv = WebDriverWait(driver, 60).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, '#verification_value'))
-    )
-    input_ccv.send_keys(ccv)
-
-
-    time.sleep(2)
-    time.sleep(2)
-
-    print("\n\n[*] Page 3/3 Done!")
-
-    driver.quit()
+  #  driver.quit()
 
 
     
